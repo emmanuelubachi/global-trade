1b0c364bbfa891efb46a9ac71ac0312fc61f8ab0
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-10-11 23:42:04 +0400

    add pages

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen w-screen flex-1">
+      <div className="w-[62vw] p-10">
+        {/* <h1 className="h1-bold">H1-Bold</h1>
+        <br />
+        <h2 className="h2-bold">H2-Bold</h2>
+        <h2 className="h2-semibold">H2-Bold</h2>
+        <br />
+        <h3 className="h3-bold">H3-Bold</h3>
+        <h3 className="h3-semibold">H3-Bold</h3> */}
+      </div>
+      <div className="w-[38vw] bg-zinc-200 p-10 dark:bg-zinc-900"></div>
+    </div>
+  );
+}
